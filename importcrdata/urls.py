1c8c76b16350b64a473c2e38574b5cae2ccf9557
Eduardo Nc <eduardonc10@gmail.com>
2019-05-21 10:47:22 -0600

    URLS modified and Ajax endpoints added

--- importcrdata/urls.py
+++ importcrdata/urls.py
@@ -1,10 +1,17 @@
-
 from django.conf.urls import url
+from django.urls import path
 from importcrdata import views
 
+app_name = 'importcrdata'
 urlpatterns = [
-    url(r'^$', views.getTseData, name='index'),
-    url('load/', views.loadDataView, name='loader'),
-#    url('view-info', views.infoViewer, name='viewer')
+    url(r'^$', views.PadronView.as_view(), name='index'),
+    url(r'estadisticas/$', views.StatisticsView.as_view(), name='estadisticas'),
+    url(r'padron/agregar_persona/$', views.PersonCreateView.as_view(), name="agregar-persona"),
+    url(r'estadisticas/provincia_ajax/$', views.ProvinciaAjaxView.as_view(), name='provincia-data'),
+    url(r'estadisticas/canton_ajax/$', views.CantonAjaxView.as_view(), name='canton-data'),
+    url(r'estadisticas/distrito_ajax/$', views.DistritoAjaxView.as_view(), name='distrito-data'),
+    path('<int:cedula>/', views.PadronDetailView.as_view(), name="detalle"),
+
+    # url('deco', views.testDecorator, name='deco'),
 
 ]
